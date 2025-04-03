2952bbfb4fbfa87e1ed95fe62368a4a911160360
FleitasEzequiel <ezequielafeita@gmail.com>
2025-04-02 23:09:38 -0300

    Primer commit

---
+++ js/app.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const cors = require("cors")
+const app = express()
+
+app.use(express.json())
+app.use(cors({
+  methods: ["GET","POST"],
+  credentials: true,
+  origin:"http://localhost:8080"
+}))
+app.use(express.urlencoded({ extended: true }));
+
+
+
+app.post("/suma",(req,res)=>{
+  const {matriz1, matriz2} = req.body
+    const resultado = matriz1.map((fila, i) => 
+      fila.map((valor, j) => valor + matriz2[i][j])
+    );
+    console.log("resultado",resultado)
+    res.send(resultado)
+})
+
+const port = 4500
+app.listen(port,()=>{
+  console.log("Servidor iniciado en el puerto", port)
+})
